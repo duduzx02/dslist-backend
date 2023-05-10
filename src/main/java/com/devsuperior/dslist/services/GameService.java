325c866a75e6027bdc220982618ce77a53518984
duduzx02 <duduzx02@gmail.com>
2023-05-10 09:56:47 -0300

    GameMindDTO, GameRepository, GameService e GameController, todas criadas e funcionando perfeitamente

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,9 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
@@ -14,11 +13,12 @@ public class GameService {
 	
 	@Autowired
 	private GameRepository gameRepository;
-	
-	public List<Game> findAll(){
-		List<Game> result = gameRepository.findAll();
-		return result;
 		
+	public List<GameMinDTO> findAll(){
+		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+
 		
 	}
 }
+
